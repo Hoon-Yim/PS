ed0ef11bedf388ec158e623ab90c3518fbc3831c
Hoon <yimsh4507@gmail.com>
2021-10-31 14:54:56 -0400

    Oct 31 2021

---
+++ easy/leetcode_206.cpp
@@ -0,0 +1,41 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <utility>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* pre = nullptr;
+        ListNode* next = nullptr;
+
+        while (head != nullptr)
+        {
+            next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+};
